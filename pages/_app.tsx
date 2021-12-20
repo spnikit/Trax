52ceeaceb2f69aa5b2992984f52d4443d8e8b145
serge <spnikit@gmail.com>
2021-12-21 01:21:57 +0300

    charka theme add

---
+++ pages/_app.tsx
@@ -0,0 +1,38 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
+
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#f5f5f5",
+      200: "eee",
+      300: "e0e0e0",
+      400: "#bdbdbd",
+      500: "#9e9e9e",
+      600: "#757575",
+      700: "#616161",
+      800: "#424242",
+      900: "#212121",
+    },
+
+    components: {
+      Button: {
+        variants: {
+          link: {
+            ":focus": { outline: "none", boxShadow: "none" },
+          },
+        },
+      },
+    },
+  },
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default MyApp;
