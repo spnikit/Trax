e90a3b2c00faa9ff16d04192dc92ef0c97267172
spnikit <spnikit@gmail.com>
2021-12-21 02:48:02 +0300

    layout

--- components/playerLayout.tsx
+++ components/playerLayout.tsx
@@ -1,10 +1,18 @@
 import { Box } from "@chakra-ui/layout";
+import Sidebar from "./sidebar";
 
 function PlayerLayout({ children }) {
   return (
-    <Box>
-      Player Layout
-      {children}
+    <Box width="100vw" height="100vh">
+      <Box position="absolute" top="0" left="0" width="250px">
+        <Sidebar />
+      </Box>
+      <Box marginLeft="250px" marginBottom="100px">
+        {children}
+      </Box>
+      <Box position="absolute" left="0" bottom="0">
+        Player
+      </Box>
     </Box>
   );
 }
