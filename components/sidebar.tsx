2a65b73627a31d7815a2f56215818080c5f61aa6
spnikit <spnikit@gmail.com>
2021-12-27 23:23:34 +0300

    sidebar complete

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -30,11 +30,18 @@ const nav = [
   },
 ];
 
+const musicMenu = [
+  { name: "Create Playlist", icon: MdPlaylistAdd, route: "/" },
+  { name: "Favourites", icon: MdFavorite, route: "/favourites" },
+];
+
+const playlists = Array.from(Array(30), (_, idx) => <span>`Playlist {idx + 1}`</span>);
+
 function Sidebar() {
   return (
     <Box width="100%" height="calc(100vh - 100px)" bg="black" paddingX="5px" color="gray">
-      <Box py="20px">
-        <Box width="120px" mb="120px" px="20px">
+      <Box py="20px" height="100%">
+        <Box width="120px" mb="20px" px="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
         <Box mb="20px">
@@ -47,6 +54,36 @@ function Sidebar() {
                       <ListIcon as={menuItem.icon} color="white" mr="20px" />
                       {menuItem.name}
                     </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box my="20px">
+          <List spacing={2}>
+            {musicMenu.map((item) => (
+              <ListItem px="20px" fontSize="16px" key={item.name}>
+                <LinkBox>
+                  <NextLink href={item.route} passHref>
+                    <LinkOverlay>
+                      <ListIcon as={item.icon} mr="20px" color="white"></ListIcon>
+                      {item.name}
+                    </LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Divider />
+        <Box height="66%" overflowY="auto" py="20px">
+          <List spacing={2}>
+            {playlists.map((list, idx) => (
+              <ListItem px="20px" key={idx}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{list}</LinkOverlay>
                   </NextLink>
                 </LinkBox>
               </ListItem>
